f0de017940509baf7589c0d544baac2bcaea2bb6
Jake <jakewheat@tutanota.com>
2022-05-22 17:27:45 +0100

    refactor build script

--- Shakefile.hs
+++ Shakefile.hs
@@ -3,22 +3,9 @@
 
 use the ./build script to run this easily
 
-todo list:
+to view the flags to the build system, and to view the build targets:
 
-make it rebuild when:
-  when the ghc version changes
-    -> it gets stuck at the moment, better than silently failing
-  when the flags for a compile or link step change,
-    including the pkg-config python3 stuff
-
-create a nice command line interface to change the build settings,
-  e.g. debug, profile, release, custom ghc flags
-
-nice way to pass args to running the tasty test exes
-
-add some CI testing for various versions of ghc, etc.
-write some tests to test this specific build script to catch
-  some regressions more easily and promptly
+./build --help
 
 -}
 
@@ -46,7 +33,7 @@ import Data.Hashable (Hashable)
 import Data.Binary (Binary)
 import Control.DeepSeq (NFData)
 
-import Control.Monad (when, void)
+import Control.Monad (when, void, forM_)
 import Development.Shake.Util
     (needMakefileDependencies
     )
@@ -59,9 +46,6 @@ testPattern = Nothing -- Just "fact"
 newtype Packages = Packages () deriving (Show,Typeable,Eq,Hashable,Binary,NFData)
 type instance RuleResult Packages = String
 
-newtype GhcVersion = GhcVersion () deriving (Show,Typeable,Eq,Hashable,Binary,NFData)
-type instance RuleResult GhcVersion = String
-
 main :: IO ()
 main = do
   shakeArgs shakeOptions{shakeFiles="_build"} $ do
@@ -70,7 +54,7 @@ main = do
 
     ------------------------------------------------------------------------------
 
-    -- main tweakables
+    -- main declarative part, making it more declaritive is a WIP
 
     let directPackages =
             ["tasty"
@@ -92,9 +76,9 @@ main = do
             ,"exceptions"
             ,"Glob"
             ]
+
     let mainPackageDB = "_build/packages/burdock-packages"
 
-        -- todo: figure out how to parameterize these into the build rules
     let objsDir = "_build/objs"
         userGhcOpts = "-v0 -hide-all-packages -package-env " ++ mainPackageDB
         srcDirs = ["src/lib"
@@ -105,7 +89,27 @@ main = do
                   ,"src/test/"]
         userCCompileOptsLoad = "pkg-config python3-embed --cflags"
         userLinkOptsLoad = "pkg-config --libs python3-embed"
-
+        pythonCFiles = ["src/pywrap/pywrap-c.c"]
+        exes = [("build the Burdock interpreter"
+                ,"_build/burdock"
+                ,"src/app/BurdockExe.hs"
+                ,pythonCFiles)
+               ,("build the Burdock tests"
+                ,"_build/burdock-tests"
+                ,"src/test/BurdockTests.hs"
+                ,pythonCFiles)
+               ,("build the Demo user FFI interpreter"
+                ,"_build/DemoFFI"
+                ,"src/examples/DemoFFI.hs"
+                ,[])
+               ]
+        allTargetExes = map (\(_,e,_,_) -> e) exes
+        simplePhonies =
+            [("build all the exes", "all", allTargetExes)
+            ,("build all the exes, build the website, run the tests"
+             ,"really-all"
+             ,["all", "website", "test"])
+            ]
 
     ------------------------------------------------------------------------------
 
@@ -137,43 +141,54 @@ main = do
                           [ofile,":",hsfile] -> p ((ofile,hsfile):acc) xs
                           _ -> error $ "didn't understand makefile line: " ++ show x
 
+    -- cache running a command and saving the output
+    -- this is for things that aren't persisted between builds
+    -- but can be memoized for a single build
+    cacheOptionsLoad <- newCache $ \(co :: String) -> do
+        Stdout (res :: String) <- cmd co
+        pure res
+    
+    -- extract all the .o file references from a dep makefile generated
+    -- by ghc -> so you pass ghc the main.hs, and then use this on the
+    -- output, to get a complete list of all the .o files from .hs files
+    --- that are needed to link that main.hs file
+    let getAllObjsFromGhcDeps makefileDepsString =
+            let deps = parseMakefileDeps makefileDepsString
+            in nub $ sort $ map fst deps
     let compileHaskellExe exeName mainSource cfiles = do
             need [mainPackageDB]
-            -- use the deps file to find out which haskell .o files
-            -- are needed to link this exe
+            -- get all the haskell files needed for linking using the deps
             let depsFile = objsDir </> mainSource <.> "deps"
             need [depsFile]
             makefileDeps <- liftIO (readFile depsFile)
-            -- parse the deps list into pairs - object file, file it depends on
-            let deps = parseMakefileDeps makefileDeps
-            let hsobjs = nub $ sort $ map fst deps
-            let cobjs = flip map cfiles $ \cfile -> objsDir </> cfile <.> "o"
-            let allObjs = hsobjs ++ cobjs
+            let hsobjs = getAllObjsFromGhcDeps makefileDeps
+                cobjs = flip map cfiles $ \cfile -> objsDir </> cfile <.> "o"
+                allObjs = hsobjs ++ cobjs
             need allObjs
-            Stdout (userLinkOpts :: String) <- cmd userLinkOptsLoad
-            --link it
+            userLinkOpts <- cacheOptionsLoad userLinkOptsLoad
             cmd_ "ghc -o" exeName allObjs userGhcOpts userLinkOpts
+
+    -- if the file exists, read it and pass to the function
+    let doIfFileExists fn f = do
+            e <- liftIO $ D.doesFileExist fn
+            when e (f =<< liftIO (readFile fn))
 
     ------------------------------------------------------------------------------
 
     -- build rules
 
-    "_build/burdock" %> \out ->
-        compileHaskellExe out "src/app/BurdockExe.hs" ["src/pywrap/pywrap-c.c"]
-    "_build/burdock-tests" %> \out ->
-        compileHaskellExe out "src/test/BurdockTests.hs" ["src/pywrap/pywrap-c.c"]
-    "_build/DemoFFI" %> \out -> compileHaskellExe out "src/examples/DemoFFI.hs" []
+    forM_ exes $ \(doc,outnm,hsmain,cfiles) ->
+        withTargetDocs doc $ outnm %> \out -> compileHaskellExe out hsmain cfiles
 
-    phony "all" $ need ["_build/burdock-tests"
-                       ,"_build/burdock"
-                       ,"_build/DemoFFI"]
+    forM_ simplePhonies $ \(doc, nm, needage) ->
+        withTargetDocs doc $ phony nm $ need needage
 
-    phony "test" $ do
+    withTargetDocs "run the tests" $ phony "test" $ do
         need ["_build/burdock-tests"]
         cmd_ "_build/burdock-tests  --color never --ansi-tricks false --hide-successes"
             (maybe "" (\x -> "-p " ++ x) testPattern)
 
-    phony "website" $ do
+    withTargetDocs "build the website" $ phony "website" $ do
         mkdirP "_build/website"
         docs <- getDirectoryFiles "" ["docs//*.rst"]
         let htmls = ["_build/website" </> dropDirectory1 (dropExtension doc) -<.> "html"
@@ -189,81 +204,76 @@ main = do
                             else ("_build/website2/style.css" : htmls2)
         need (websiteFiles ++ website2Files)
 
-    phony "really-all" $ need ["all", "website", "test"]
+    withTargetDocs "clean everything that any of the targets creates"
+        $ phony "clean" $ do
+        liftIO $ removeFiles "." ["_build", "dist-newstyle", "src/pywrap/dist-newstyle"]
 
-    phony "clean" $ do
-        removeFilesAfter "_build" ["//*"]
-        liftIO $ removeFiles "." ["dist-newstyle", "src/pywrap/dist-newstyle"]
-
-    mainPackageDB %> \out -> do
+    withTargetDocs "build the cabal package db for the haskell code"
+        $ mainPackageDB %> \out -> do
         void $ askOracle (Packages ())
-        void $ askOracle (GhcVersion ())
         installPackageDB out directPackages
 
-    "_build//*.hs.deps" %> \out -> do
+    withTargetDocs "auto haskell deps" $
+        "_build//*.hs.deps" %> \out -> do
         need [mainPackageDB]
         -- the dep file needs rebuilding if any of the .hs files it
         -- references have changed
-        -- what's the right idiom for this?
-        e <- liftIO $ D.doesFileExist out
-        when e $ do
-            makefileDeps <- liftIO (readFile out)
+        -- is this the right idiom for this?
+        doIfFileExists out $ \makefileDeps -> 
             let hsFileDeps = filter (".hs" `isSuffixOf`) $ map snd $ parseMakefileDeps makefileDeps
-            need hsFileDeps
+            in need hsFileDeps
 
         -- generate the deps file from ghc
         let hsFile = makeRelative objsDir (dropExtension out)
             ghcdepsfile = out <.> "tmp"
         mkdirP (takeDirectory out)
-        void $ askOracle (GhcVersion ())
         cmd_ "ghc -M" hsFile "-dep-makefile" ghcdepsfile "-dep-suffix=hs." userGhcOpts srcDirOption
         makefileDeps <- liftIO (readFile ghcdepsfile)
+        -- todo: helper function for this:
         let deps = parseMakefileDeps makefileDeps
-        let g fn = if ".hs" `isSuffixOf` fn
+            g fn = if ".hs" `isSuffixOf` fn
                    then fn
                    else objsDir </> fn
             deps' = flip map deps $ \(o,f) -> (g o, g f)
             newdeps = unlines $ flip map deps' $ \(a,b) -> unwords [a,":", b]
-        -- liftIO $ putStrLn $ "new deps:\n" ++ newdeps
+        ----
         liftIO $ writeFile out newdeps
                 
-    ["_build//*.hs.o","_build//*.hs.hi"] &%> \case
-      [out,_] -> do
-        need [mainPackageDB]
-        let hsfile = (dropExtension $ dropExtension $ makeRelative objsDir out) <.> "hs"
-            depsfile = dropExtension out <.> "deps"
-        need [depsfile]
-        needMakefileDependencies depsfile
-        
-        let hifile = (dropExtension out) <.> "hi"
-        let d = takeDirectory out
-        mkdirP d
-        void $ askOracle (GhcVersion ())
-        cmd_ "ghc -hisuf .hs.hi -c" hsfile "-o" out "-ohi" hifile userGhcOpts srcDirOption
-      _ -> error "impossible build failure: matched two items in rule but didn't get two items to build"
+    withTargetDocs "compile hs file" $ ["_build//*.hs.o","_build//*.hs.hi"] &%> \case
+        [out,_] -> do
+            need [mainPackageDB]
+            let hsfile = (dropExtension $ dropExtension $ makeRelative objsDir out) <.> "hs"
+                depsfile = dropExtension out <.> "deps"
+            need [depsfile]
+            needMakefileDependencies depsfile
+            let hifile = (dropExtension out) <.> "hi"
+                d = takeDirectory out
+            mkdirP d
+            cmd_ "ghc -hisuf .hs.hi -c" hsfile "-o" out "-ohi" hifile userGhcOpts srcDirOption
+        _ -> error "impossible build failure: matched two items in rule but didn't get two items to build"
 
-    "_build//*.c.o" %> \out -> do
+    withTargetDocs "compile c file" $ "_build//*.c.o" %> \out -> do
         let cfile = (dropExtension $ dropExtension $ makeRelative objsDir out) <.> "c"
         need [cfile]
-        Stdout (userCCompileOpts :: String) <- cmd userCCompileOptsLoad
+        userCCompileOpts <- cacheOptionsLoad userCCompileOptsLoad
         mkdirP (takeDirectory out)
         cmd_ "gcc" cfile "-c -o" out userCCompileOpts
 
-    "_build/website/style.css" %> \out -> do
-        copyFile' "docs/style.css" out
+    withTargetDocs "copy website file" $
+        "_build/website/style.css" %> \out -> copyFile' "docs/style.css" out
 
-    "_build/website/*.html" %> \out -> do
-        let rst = "docs" </> dropDirectory1 (dropDirectory1 $ dropExtension out -<.> "rst")
-        need [rst, "docs/header.html"]
-        doPandoc "docs/header.html" rst out
+    withTargetDocs "build website html from rst" $
+        "_build/website/*.html" %> \out -> do
+            let rst = "docs" </> dropDirectory1 (dropDirectory1 $ dropExtension out -<.> "rst")
+            need [rst, "docs/header.html"]
+            doPandoc "docs/header.html" rst out
 
-    "_build/website2/style.css" %> \out -> do
-        copyFile' "website2/style.css" out
+    withTargetDocs "copy website file" $
+        "_build/website2/style.css" %> \out -> copyFile' "website2/style.css" out
 
-    "_build/website2/*.html" %> \out -> do
+    withTargetDocs "build website html from rst" $ "_build/website2/*.html" %> \out -> do
         let rst = "website2" </> dropDirectory1 (dropDirectory1 $ dropExtension out -<.> "rst")
         need [rst, "website2/header.html"]
         doPandoc "website2/header.html" rst out
 
-    void $ addOracle $ \(GhcVersion _) -> fromStdout <$> cmd "ghc --numeric-version" :: Action String
     void $ addOracle $ \(Packages _) -> pure (unlines directPackages) :: Action String
