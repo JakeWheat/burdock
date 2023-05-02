2e2fe1d45a6fd527a745b55cc20f03311a2b1594
Jake Wheat <jakewheat@tutanota.com>
2023-05-02 18:14:48 +0100

    new attribute grammar system in desugar

--- src/haskell/lib/Burdock/Desugar.hs
+++ src/haskell/lib/Burdock/Desugar.hs
@@ -6,6 +6,8 @@ syntax, to the properly abstract ast which the interpreter runs on
 
 this code also adds tags for the free vars in lambda bodies
 
+uses a simple attribute grammar system
+
 later it will also be where the lexical binding checks and static type
 checker run
 
@@ -13,6 +15,10 @@ checker run
 
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Burdock.Desugar
     (desugar
     ,prelude
@@ -25,23 +31,122 @@ import Burdock.Utils (error, show)
 import qualified Burdock.Syntax as S
 import qualified Burdock.InterpreterSyntax as I
 import Data.Text (Text)
---import qualified Data.Text as T
 import qualified Data.Text.Lazy as L
 
 import Burdock.Pretty (prettyExpr)
-import Data.List (nub,sort)
+import Data.List
+    (nub
+    ,(\\)
+    )
 
 import Burdock.DefaultRuntime (prelude)
+
+import Control.Monad.Reader
+    (runReader
+    ,Reader
+    --,ask
+    ,asks
+    ,local
+    )
+
+import Lens.Micro.TH
+    (makeLenses
+    --,makeFields
+    )
+import Lens.Micro
+    (over
+    ,set
+    )
+
+import Lens.Micro.Extras
+    (view)
+
+------------------------------------------------------------------------------
+
+{-
+
+Inh is the inherited attributes
+
+These go from nodes to children nodes, the Inh value is passed around
+the tree in a Reader monad
+
+The code uses local to update the Inh values in the tree
+
+Syn are the synthesizes attributes
+
+These go from nodes to parent nodes. They are passed around the tree
+in the return values to the desugar functions.
+
+TODO: explain the different fields in these two data types.
+
+explain the logic of how they are changed? with helpers, or
+does this go in the individual nodes
+
+-}
+
+data Inh
+    = Inh
+    {_inhCtors :: [Text]}
+
+data Syn e
+    = Syn
+    {_synTree :: e
+    ,_synFreeVars :: [Text]
+    ,_synNewBindings :: [Text]
+    ,_synNewCtors :: [Text]
+    }
+    deriving (Show, Functor)
+
+makeLenses ''Inh
+makeLenses ''Syn
+
+type Desugar = Reader Inh
+
+combineSyns :: [Syn ()] -> Syn a -> Syn a
+combineSyns as a =
+    set synFreeVars (nub $ concat (view synFreeVars a : map (view synFreeVars) as)) a
+
+-- future proofing, not sure if worth it
+combineSynsNoFreeVars :: [Syn ()] -> Syn a -> Syn a
+combineSynsNoFreeVars _as a = a
+
+combineSynsWithNewBindings :: [Syn ()] -> Syn a -> Syn a
+combineSynsWithNewBindings as a =
+    set synFreeVars (nub $ concat (view synFreeVars a : map (view synFreeVars) as))
+    $ set synNewBindings (concat (view synNewBindings a : map (view synNewBindings) as))
+    a
+
+ns :: Syn a -> Syn ()
+ns = fmap (const ())
+
+mkSyn :: e -> Syn e
+mkSyn e = Syn e [] [] []
 
 ------------------
 
 desugar :: S.Script -> [I.Stmt]
-desugar (S.Script ss) = desugarStmts ss
+desugar (S.Script ss) =
+    view synTree $ runReader (desugarStmts ss) (Inh [])
+ 
+desugarStmts :: [S.Stmt] -> Desugar (Syn [I.Stmt])
 
-desugarStmts :: [S.Stmt] -> [I.Stmt]
-desugarStmts ss = concatMap desugarStmt ss
+desugarStmts [] = pure $ mkSyn []
 
-desugarStmt :: S.Stmt -> [I.Stmt]
+desugarStmts (s:ss) = do
+    s' <- desugarStmt s
+    let addCtors = over inhCtors (view synNewCtors s' ++)
+    ss' <- local addCtors $ desugarStmts ss
+
+    -- get the free vars from ss
+    -- remove the newbindings from s
+    -- pass this up the chain
+    let remainingFreeVars = nub $ (view synFreeVars ss' \\ view synNewBindings s') ++ view synFreeVars s'
+
+    pure $ set synFreeVars remainingFreeVars
+         $ combineSynsNoFreeVars [ns s', ns ss']
+         $ mkSyn (_synTree s' ++ _synTree ss')
+
+desugarStmt :: S.Stmt -> Desugar (Syn [I.Stmt])
 
 ------------------
 
@@ -102,9 +207,13 @@ check-variants-equal(on-fields :: list<string>,a,b)
 
 
 -}
-desugarStmt (S.DataDecl _ dnm _ vs shr Nothing) =
-    desugarStmts $ concatMap varF vs ++ [isDat]
+desugarStmt (S.DataDecl _ dnm _ vs shr Nothing) = do
+    let --newCtors
+        addNewCtors = over synNewCtors (newCtors ++)
+    addNewCtors <$> (desugarStmts $ concatMap varF vs ++ [isDat])
   where
+    newCtors = flip map vs $ \case
+        S.VariantDecl _ nm _ _ -> nm
     varF (S.VariantDecl _ vnm bs meths) =
         let extraMeths ps =
                 [(S.Text n "_equals"
@@ -157,26 +266,34 @@ desugarStmt (S.DataDecl _ dnm _ vs shr Nothing) =
 -- these are the functions which contains the code that actually
 -- converts to the I syntax
 
-desugarStmt (S.LetDecl _ b e) =
-    let nm = desugarBinding b
-    in [I.LetDecl nm $ desugarExpr e]
+desugarStmt (S.LetDecl _ b e) = do
+    nm <- desugarBinding b
+    e' <- desugarExpr e
+    pure $ set synNewBindings (view synNewBindings nm)
+        $ combineSyns [ns nm, ns e']
+        $ mkSyn [I.LetDecl (_synTree nm) $ _synTree e']
 
-desugarStmt (S.StmtExpr _ e) = [I.StmtExpr $ desugarExpr e]
+desugarStmt (S.StmtExpr _ e) = do
+    e' <- desugarExpr e
+    pure $ combineSyns [ns e'] $ mkSyn [I.StmtExpr $ _synTree e']
 
-desugarStmt (S.VarDecl _ (S.SimpleBinding _ _ nm _) e) =
-    [I.VarDecl nm $ desugarExpr e]
+desugarStmt (S.VarDecl _ (S.SimpleBinding _ _ nm _) e) = do
+    e' <- desugarExpr e
+    pure $ set synNewBindings [nm]
+        $ combineSyns [ns e']
+        $ mkSyn [I.VarDecl nm $ _synTree e']
 
-desugarStmt (S.SetVar _ (S.Iden _ nm) e) =
-    [I.SetVar nm $ desugarExpr e]
+desugarStmt (S.SetVar _ (S.Iden _ nm) e) = do
+    e' <- desugarExpr e
+    pure $ combineSyns [ns e'] $ mkSyn [I.SetVar nm $ _synTree e']
 
 desugarStmt x = error $ "desugarStmt " <> show x
 
 ---------------------------------------
 
-desugarExpr :: S.Expr -> I.Expr
+desugarExpr :: S.Expr -> Desugar (Syn I.Expr)
 
 ------------------
-
 -- S -> S
 
 desugarExpr (S.TupleGet sp v n) =
@@ -213,7 +330,8 @@ desugarExpr (S.BinOp _ e1 op e2) | Just op' <- lookup op methOps =
     
 desugarExpr (S.Let _ bs e) =
     desugarExpr $ S.Block Nothing
-        $ (map (\(b,e1) -> S.LetDecl Nothing b e1) bs) ++ e -- [S.StmtExpr Nothing e]
+    $ (map (\(b,e1) -> S.LetDecl Nothing b e1) bs) ++ e
+
 desugarExpr (S.Parens _ e) = desugarExpr e
 
 desugarExpr (S.Construct _ ["list"] es) =
@@ -222,68 +340,124 @@ desugarExpr (S.Construct _ ["list"] es) =
 ------------------
 -- S -> I
 
-desugarExpr (S.Block _ ss) = I.Block $ desugarStmts ss
+desugarExpr (S.Block _ ss) = do
+    ss' <- desugarStmts ss
+    pure $ combineSyns [ns ss'] $ mkSyn $ I.Block $ _synTree ss'
 
-desugarExpr (S.If _ ts els) =
-    I.If (map f ts) (desugarStmts <$> els)
+desugarExpr (S.If _ ts els) = do
+    ts' <- mapM f ts
+    els' <- g els
+    -- there has to be an easier way to do this
+    let stTs = flip map ts' $ (\(a,b) -> (_synTree a, _synTree b))
+        stEls = case els' of
+                   Nothing -> Nothing
+                   Just x -> Just $ _synTree x
+        cs = combineSyns $
+             flip concatMap ts' (\(a,b) -> [ns a, ns b])
+             ++ (maybe [] ((:[]) . ns) els')
+    pure $ cs $ mkSyn $ I.If stTs stEls
   where
-    f (a,b) = (desugarExpr a, desugarStmts b)
+    g Nothing = pure Nothing
+    g (Just x) = Just <$> desugarStmts x
+    f (a,b) = (,) <$> desugarExpr a <*> desugarStmts b
 
-desugarExpr (S.DotExpr _ e fld) =
-    I.DotExpr (desugarExpr e) fld
+desugarExpr (S.DotExpr _ e fld) = do
+    e' <- desugarExpr e
+    pure $ combineSyns [ns e'] $  mkSyn $ I.DotExpr (_synTree e') fld
      
-desugarExpr (S.RecordSel _ fs) =
-    let trm = ("_torepr",desugarExpr $ S.Iden n "_record_torepr")
-        eqm = ("_equals",desugarExpr $ S.Iden n "_record_equals")
-    in I.VariantSel "record" (trm : eqm : map (\(a,b) -> (a, desugarExpr b)) fs)
+desugarExpr (S.RecordSel _ fs) = do
+    trm <- ("_torepr",) <$> desugarExpr (S.Iden n "_record_torepr")
+    eqm <- ("_equals",) <$> desugarExpr (S.Iden n "_record_equals")
+    fs' <- mapM (\(a,b) -> (a,) <$> desugarExpr b) fs
+    let fs'' = trm : eqm : fs'
+        addHelpers = over synFreeVars (nub . (\x -> "_record_torepr" : "_record_equals" : x))
+    pure $ addHelpers $ combineSyns (map (ns . snd) fs'')
+        $ mkSyn $ I.VariantSel "record" $ map stp fs''
+        
   where
+    stp (a,b) = (a,_synTree b)
     n = Nothing
 
-desugarExpr (S.TupleSel _ fs) =
+desugarExpr (S.TupleSel _ fs) = do
+    trm <- ("_torepr",) <$> desugarExpr (S.Iden n "_tuple_torepr")
+    eqm <- ("_equals",) <$> desugarExpr (S.Iden n "_tuple_equals")
     let fs1 = zip (map show [(0::Int)..]) fs
-        trm = ("_torepr",desugarExpr $ S.Iden n "_tuple_torepr")
-        eqm = ("_equals",desugarExpr $ S.Iden n "_tuple_equals")
-    in I.VariantSel "tuple" (trm : eqm : map (\(a,b) -> (a, desugarExpr b)) fs1)
+    fs' <- mapM (\(a,b) -> (a,) <$> desugarExpr b) fs1
+    let fs'' = trm : eqm : fs'
+        addHelpers = over synFreeVars (nub . (\x -> "_record_torepr" : "_record_equals" : x))
+    let x = addHelpers $ combineSyns (map (ns . snd) fs'')
+            $ mkSyn $ I.VariantSel "tuple" $ map stp fs''
+    pure x
   where
+    stp (a,b) = (a,_synTree b)
     n = Nothing
 
-desugarExpr (S.App _ (S.Iden _ "run-task-cs") [e]) =
-    I.RunTask False $ desugarExpr e
+desugarExpr (S.App _ (S.Iden _ "run-task-cs") [e]) = do
+    e' <- desugarExpr e
+    pure $ combineSyns [ns e'] $ mkSyn $ I.RunTask False (_synTree e')
 
-desugarExpr (S.App _ (S.Iden _ "run-task-cs-async") [e]) =
-    I.RunTask True $ desugarExpr e
+desugarExpr (S.App _ (S.Iden _ "run-task-cs-async") [e]) = do
+    e' <- desugarExpr e
+    pure $ combineSyns [ns e'] $ mkSyn $ I.RunTask True (_synTree e')
 
-desugarExpr (S.App sp f es) =
+desugarExpr (S.App sp f es) = do
     let spx = case sp of
             Nothing -> "nothing"
             Just (n,i,j) -> "(" <> n <> "," <> show i <> "," <> show j <> ")"
-    in I.App (Just spx) (desugarExpr f) $ map desugarExpr es
+    f' <- desugarExpr f
+    es' <- mapM desugarExpr es
+    pure $ combineSyns (ns f' : map ns es')
+        $ mkSyn $ I.App (Just spx) (_synTree f') (map _synTree es')
 
-desugarExpr (S.Lam _ (S.FunHeader _ bs _) bdy) =
-    let bdy' = desugarStmts bdy
-        pnms = map desugarBinding bs
-        pnmsx = concatMap getBindingNames pnms
-        -- adding names as a temp hack before capturing closures properly
-        -- in all the bootstrap and built in code
-        fv = nub $ sort ("_record_torepr" : "_record_equals" : freeVarsStmts pnmsx bdy')
-    in I.Lam fv pnms bdy'
+desugarExpr (S.Lam _ (S.FunHeader _ bs _) bdy) = do
+    bdy' <- desugarStmts bdy
+    bs' <- mapM desugarBinding bs
+    -- get the freevars from desugar bindings to pass up
+    -- get the freevars from body to pass up
+    --   remove the newbindings from bindings before passing up
+    -- how to refactor this into a nice helper?
+    let nb = concatMap (view synNewBindings) bs'
+        bodyFreeVars = view synFreeVars bdy' \\ nb
+        bindingsFreeVars = concatMap (view synFreeVars) bs'
+        -- add a few bonus things for now, later will get the closure capture
+        -- sorted and remove these
+        allFreeVars = nub $ bodyFreeVars ++ bindingsFreeVars ++ ["make-list", "make-variant", "left", "right"]
+        setFreeVars = set synFreeVars allFreeVars
+    pure $ setFreeVars
+         $ combineSynsNoFreeVars (map ns bs' ++ [ns bdy'])
+         $ mkSyn $ I.Lam allFreeVars (map _synTree bs') (_synTree bdy')
 
-desugarExpr (S.Text _ s) = I.IString s
-desugarExpr (S.Num _ s) = I.Num s
+desugarExpr (S.Text _ s) = pure $ mkSyn $ I.IString s
+desugarExpr (S.Num _ s) = pure $ mkSyn $ I.Num s
 
-desugarExpr (S.Iden _ i) = I.Iden i
-
-desugarExpr (S.MethodExpr _ (S.Method (S.FunHeader _ts (a:as) _mty) bdy))
-    = I.MethodExpr $ desugarExpr (S.Lam Nothing (S.FunHeader [] [a] Nothing)
+-- iden
+desugarExpr (S.Iden _ i) =
+    pure (mkSyn $ I.Iden i) {_synFreeVars = [i]}
+-- methods
+desugarExpr (S.MethodExpr _ (S.Method (S.FunHeader _ts (a:as) _mty) bdy)) = do
+    e <- desugarExpr (S.Lam Nothing (S.FunHeader [] [a] Nothing)
                         [S.StmtExpr Nothing $ S.Lam Nothing (S.FunHeader [] as Nothing) bdy])
+    pure $ combineSyns [ns e] $  mkSyn $ I.MethodExpr (_synTree e)
 
-desugarExpr (S.Cases _ tst _ bs mels) =
-    let tst' = desugarExpr tst
-        bs' = flip map bs $ \(bm,_,bdy) -> (desugarBinding bm, desugarStmts bdy)
-        mels' = case mels of
-                    Nothing -> [] -- todo: insert explicit raise here
-                    Just bdy -> [(I.WildcardBinding, desugarStmts bdy)]
-    in I.Cases tst' (bs' ++ mels')
+desugarExpr (S.Cases pos tst _ bs mels) = do
+    tst' <- desugarExpr tst
+    let bsx = bs ++ case mels of
+            Nothing -> [(S.NameBinding Nothing "asdfsdfs", Nothing
+                        ,[S.StmtExpr n $ S.App n (S.Iden n "raise")
+                          [S.BinOp n (S.Text n (show pos <> " cases no branch matched: ")) "+"
+                              (S.App n (S.Iden n "torepr") [S.Iden n "asdfsdfs"])]])]
+            Just el -> [(S.WildcardBinding Nothing, Nothing, el)]
+    bs' <- mapM desugarBranch bsx
+    pure $ combineSyns (ns tst' : map ns bs')
+         $ mkSyn $ I.Cases (_synTree tst') (map _synTree bs')
+  where
+    desugarBranch (bm,_,bdy) = do
+        bm' <- desugarBinding bm
+        bdy' <- desugarStmts bdy
+        pure $ set synFreeVars (nub (view synFreeVars bdy' \\ view synNewBindings bm'))
+            $ combineSynsNoFreeVars [ns bm', ns bdy']
+            $ mkSyn (_synTree bm', _synTree bdy')
+    n = Nothing
 
 desugarExpr x = error $ "desugarExpr " <> show x
 
@@ -311,62 +485,32 @@ doLetRec bs =
 
 ---------------------------------------
 
-desugarBinding :: S.Binding -> I.Binding
+desugarBinding :: S.Binding -> Desugar (Syn I.Binding)
 desugarBinding = \case
-    S.NameBinding _ nm -> I.NameBinding nm
-    S.ShadowBinding _ nm -> I.NameBinding nm
-    S.VariantBinding _ [vnm] bs -> I.VariantBinding vnm $ map desugarBinding bs
-    S.WildcardBinding _ -> I.WildcardBinding
-    S.TupleBinding _ bs -> I.TupleBinding $ map desugarBinding bs
+    S.NameBinding _ nm -> do
+        x <- isCtor nm
+        -- if it's a no arg ctor, convert to variant binding
+        if x
+            then addName nm $ mkSyn (I.VariantBinding nm [])
+            else addName nm $ mkSyn (I.NameBinding nm)
+        --addName nm $ mkSyn (I.NameBinding nm)
+    S.ShadowBinding _ nm -> addName nm $ mkSyn (I.NameBinding nm)
+    S.VariantBinding _ [vnm] bs -> do
+        bs' <- mapM desugarBinding bs
+        addName vnm $ combineSynsWithNewBindings (map ns bs') $ mkSyn $ I.VariantBinding vnm $ map _synTree bs'
+    S.WildcardBinding _ -> pure $ mkSyn $ I.WildcardBinding
+    S.TupleBinding _ bs -> do
+        -- combine the subbindings
+        bs' <- mapM desugarBinding bs
+        pure $ combineSynsWithNewBindings (map ns bs') $ mkSyn $ I.TupleBinding $ map _synTree bs'
     x -> error $ "unsupported binding: " <> show x
-    
-getBindingNames :: I.Binding -> [Text]
-getBindingNames (I.NameBinding nm) = [nm]
-getBindingNames I.WildcardBinding = []
-getBindingNames (I.VariantBinding _ bs) = concatMap getBindingNames bs
-getBindingNames (I.TupleBinding bs) = concatMap getBindingNames bs
-
-freeVarsExpr :: [Text] -> I.Expr -> [Text]
-freeVarsExpr bs (I.Block sts) = freeVarsStmts bs sts
-
-freeVarsExpr bs (I.DotExpr e _) = freeVarsExpr bs e
-
-freeVarsExpr bs (I.App _ f es) = freeVarsExpr bs f ++ concatMap (freeVarsExpr bs) es
-
-freeVarsExpr bs (I.Iden i) = if i `elem` bs
-                             then []
-                             else [i]
-
-freeVarsExpr _bs (I.IString {}) = []
-freeVarsExpr _bs (I.Num {}) = []
-freeVarsExpr bs (I.If ts els) =
-    concatMap (\(a,b) -> freeVarsExpr bs a ++ freeVarsStmts bs b) ts
-    ++ maybe [] (freeVarsStmts bs) els
-
-freeVarsExpr _bs (I.Lam fv _as _bdy) = fv
-freeVarsExpr bs (I.MethodExpr e) = freeVarsExpr bs e
-freeVarsExpr bs (I.RunTask _ e) = "left" : "right" : freeVarsExpr bs e
-
-freeVarsExpr bs (I.Cases e ts) =
-    freeVarsExpr bs e ++ concatMap fv ts
   where
-    fv (p,s) = let nbs = getBindingNames p
-               in freeVarsStmts (nbs ++bs) s
-
-freeVarsExpr bs (I.VariantSel _ fs) =
-    concatMap (freeVarsExpr bs . snd) fs
-
---freeVarsExpr _ e = error $ "freeVarsExpr: " <> show e
-
-freeVarsStmts :: [Text] -> [I.Stmt] -> [Text]
-freeVarsStmts _bs [] = []
-freeVarsStmts bs (I.StmtExpr e : ss) =
-    freeVarsExpr bs e ++ freeVarsStmts bs ss
-
-freeVarsStmts bs (I.LetDecl nm e : ss) =
-    freeVarsExpr bs e
-    ++ let bbs = getBindingNames nm ++ bs
-       in freeVarsStmts bbs ss
-
-freeVarsStmts _ (s:_) = error $ "freeVarsStmts: " <> show s
-
+    addName nm b = do
+        x <- isCtor nm
+        -- if nm is a ctor add to freevars otherwise add to newbindings
+        let l = if x
+                then synFreeVars
+                else synNewBindings
+        pure $ over l (nm:) b
+    isCtor :: Text -> Desugar Bool
+    isCtor nm = asks ((nm `elem`) .  view inhCtors)
